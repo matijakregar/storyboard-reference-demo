31e7a7793b4e0a9b6f0ac2f98aa2203687e7d966
Matija Kregar <matija.kregar@gmail.com>
2016-10-31 15:27:58 +0100

    Extend UIImageView to load external images.

---
+++ StoryboardRefDemo/UIImageView.swift
@@ -0,0 +1,25 @@
+//
+//  UIImageView.swift
+//  StoryboardRefDemo
+//
+//  Created by Matija Kregar on 31/10/2016.
+//  Copyright © 2016 Matija Kregar. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    public func imageFromURL(_ url: URL) {
+        
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> Void in
+            if error != nil {
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.image = image
+            })
+            
+        }).resume()
+    }}
